2eb7a338f28fa01adc24e88488652e81fc982384
Ning <ning@zervice.us>
2026-06-24 11:32:38 +0000

    Improve navigation

--- scripts/startnav.sh
+++ scripts/startnav.sh
@@ -2,4 +2,4 @@
 
 # 启动自动驾驶仪
 source ~/work/ros2/r550/install/setup.bash
-ros2 launch r550_description r550_nav2.launch.py
+ros2 launch r550_description r550_nav2.launch.py | tee ~/work/ros2/r550/nav.log
--- scripts/startsim.sh
+++ scripts/startsim.sh
@@ -2,4 +2,4 @@
 
 # 拉起物理仿真世界
 source ~/work/ros2/r550/install/setup.bash
-ros2 launch r550_description r550_sim.launch.py
+ros2 launch r550_description r550_sim.launch.py | tee ~/work/ros2/r550/sim.log
